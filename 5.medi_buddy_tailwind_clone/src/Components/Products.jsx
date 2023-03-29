fa0eee91bf973f8906f2ce0a01586e6f5d9eb5af
maheshsarade001 <maheshsarade001@gmail.com>
2023-03-29 20:48:18 +0530

    Added navbar-2 in Products component

--- 5.medi_buddy_tailwind_clone/src/Components/Products.jsx
+++ 5.medi_buddy_tailwind_clone/src/Components/Products.jsx
@@ -16,28 +16,56 @@ const Products = () => {
     <>
       <div className="max-w-screen-xl mx-auto">
         <div>
-          <div className="flex justify-around max-w-4xl mx-auto">
-            <div className="bg-gray-400">
+          <div className="flex justify-around items-center max-w-4xl mx-auto mb-7">
+            <div
+              style={{ backgroundImage: `url(${svga})` }}
+              className="bg-[#f5f5f5] px-5 pt-3 pb-6 bg-[length:10px] bg-repeat rounded-3xl shadow-[0px_0px_15px_rgba(24,24,24,0.161)] "
+            >
               <img src={TalkToDoctor} alt="TalktoDoctor" />
-              <img src={svga} alt="" />
+              <p className="text-sm text-[#050a4e] font-bold my-1 mx-auto">
+                Talk To Doctor
+              </p>
             </div>
             <div>
               <img src={Medicine} alt="Medicine" />
+              <p className="text-sm text-[#050a4e] font-bold my-1 mx-auto">
+                Medicine
+              </p>
             </div>
             <div>
               <img src={SurgeryP} alt="Surgery" />
+              <p className="text-sm text-[#050a4e] font-bold my-1 mx-auto">
+                Surgery
+              </p>
             </div>
             <div>
               <img src={MBGold} alt="MedyBuddy Gold" />
+              <p className="text-sm text-[#050a4e] font-bold my-1 mx-auto">
+                <span>Medi</span>
+                <span>Buddy</span>
+                <br />
+              </p>
+              <p className="text-sm text-[#050a4e] font-bold my-1 mx-auto">
+                GOLD
+              </p>
             </div>
             <div>
               <img src={Insurance} alt="Insurance" />
+              <p className="text-sm text-[#050a4e] font-bold my-1 mx-auto">
+                Insurance
+              </p>
             </div>
             <div>
               <img src={LabTest} alt="Lab & Test" />
+              <p className="text-sm text-[#050a4e] font-bold my-1 mx-auto">
+                Lab Test & <br /> Packages
+              </p>
             </div>
             <div>
               <img src={More} alt="More" />
+              <p className="text-sm text-[#050a4e] font-bold my-1 mx-auto">
+                More
+              </p>
             </div>
           </div>
         </div>
